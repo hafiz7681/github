d2a66f7955d7ed5ed059aa32f6ea2a74964ac048
hafiz7681 <github7681@gmail.com>
2024-11-19 17:16:00 +0700

    array2.php

---
+++ apache2/array2.php
@@ -0,0 +1,27 @@
+<?php
+  $mahasiswa = [
+    ["sandhika", "043040023", "teknik informatika", "sandhikagalih@unpas.ac.id"],
+    ["doddy ferdiansyah", "033040001", "teknik industri", "doddy@yahoo.com"],
+    ["erik", "023040123", "teknik planalogi", "erik@gmail.com"]
+  ];
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>daftar mahasiswa</title>
+  </head>
+  <body>
+    
+    <h1>daftar mahasiswa</h1>
+    
+    <?php foreach( $mahasiswa as $mhs ) : ?>
+    <ul>
+      <li>nama : <?= $mhs[0]; ?></li>
+      <li>nrp : <?= $mhs[1]; ?></li>
+      <li>jurusan : <?= $mhs[2]; ?></li>
+      <li>email : <?= $mhs[3]; ?></li>
+    </ul>
+    <?php endforeach; ?>
+    
+  </body>
+</html>
