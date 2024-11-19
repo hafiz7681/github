0d5568303b07e413d8c4a2607723b5934ae6ca45
hafiz7681 <github7681@gmail.com>
2024-11-19 16:02:32 +0700

    ada perubahan

--- apache2/array.php
+++ apache2/array.php
@@ -21,8 +21,40 @@
   $hari[] = "jumat";
   echo "<br />";
   var_dump($hari);
-  // lanjutan
   
-  
-  
+  echo "<br/><br/>";
+  // latihan array2
+  // menggunakan foreach
+  $angka = [3,2,15,20,11,77,89,8];
 ?>
+<!doctype html>
+<html>
+  <head>
+    <title>latihan 2</title>
+    <style>
+      .kotak {
+        width: 50px;
+        height: 50px;
+        background-color: salmon;
+        text-align: center;
+        line-height: 50px;
+        margin: 3px;
+        float: left;
+      }
+      .clear { clear: both; }
+    </style>
+  </head>
+  <body>
+    
+    <?php for( $i = 0; $i < count($angka); $i++ ) : ?>
+    <div class="kotak"><?= $angka[$i]; ?></div>
+    <?php endfor; ?>
+  
+    <div class="clear"></div>
+  
+  <?php foreach( $angka as $a ) : ?>
+    <div class="kotak"><?= $a; ?></div>
+  <?php endforeach; ?>
+  
+  </body>
+</html>
