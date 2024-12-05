48b8e886137b82b489561845d7ada6b64269ac49
hafiz7681 <github7681@gmail.com>
2024-12-05 10:36:46 +0700

    perubahan file

--- apache2/get_post/get_post3/admin.php
+++ apache2/get_post/get_post3/admin.php
@@ -3,11 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>POST</title>
+	<title>Halaman Admin</title>
 </head>
 <body>
 
-	<h1>Selamat Datang, <?= $_POST["nama"] ?>!</h1>
+	<h1>Selamat Datang, Admin!</h1>
+
+	<a href="login.php">Logout</a>
 
 </body>
 </html>
