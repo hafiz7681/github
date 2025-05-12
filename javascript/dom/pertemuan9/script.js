4b34300367fc1600516e635d267f079da1fddf41
hafiz7681 <github7681@gmail.com>
2025-05-12 16:33:31 +0700

    new

--- javascript/dom/pertemuan9/script.js
+++ javascript/dom/pertemuan9/script.js
@@ -2,7 +2,9 @@ var container = document.querySelector('.container');
 
 container.addEventListener('click', function(e) {
     if(e.target.className == 'close') {
+        // e.target mengambil element yang di klik
         e.target.parentElement.remove();
+        
         e.preventDefault();
     }
 });
