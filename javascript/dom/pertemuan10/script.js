9297d3f97d820f61017626a27cb6fd17eb34db45
hafiz7681 <github7681@gmail.com>
2025-05-15 22:23:53 +0700

    new

--- javascript/dom/pertemuan10/script.js
+++ javascript/dom/pertemuan10/script.js
@@ -5,18 +5,18 @@ const thumbs = document.querySelectorAll('.thumb');
 container.addEventListener('click', function(e) {
 	if(e.target.className == 'thumb') {
 		jumbo.setAttribute('src', e.target.getAttribute('src'));
-		jumbo.classList.add('fade');
+		/*jumbo.classList.add('fade');
 		setTimeout(function() {
 			jumbo.classList.remove('fade');
 		}, 500);
 		
 		thumbs.forEach(function(thumb) {
-			/*if(thumb.classList.contains('active')) {
+			if(thumb.classList.contains('active')) {
 				thumb.classList.remove('active');
-			}*/
-			thumb.className = 'thumb';
+			}
+			// thumb.className = 'thumb';
 		});
 
-		e.target.classList.add('active');
+		e.target.classList.add('active');*/
 	}
 });
