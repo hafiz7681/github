4d6506a68aaa35349444f5e7b8ee5fb685a1d58f
hafiz7681 <github7681@gmail.com>
2025-05-19 00:32:56 +0700

    new

---
+++ javascript/js_advanced/pertemuan2/script.js
@@ -0,0 +1,101 @@
+// Membuat Object pada JavaScript
+// 1. Object Literal
+let mahasiswa1 = {
+	nama: 'sandhika',
+	energi: 10,
+	makan: function(makan) {
+		this.energi += makan;
+		console.log(Mahasiswa1);
+	},
+	main: function(main) {
+		this.energi -= main;
+		console.log(Mahasiswa1);
+	}
+}
+
+let mahasiswa2 = {
+	nama: 'doddy',
+	energi: 20,
+	makan: function(makan) {
+		this.energi += makan;
+		console.log(Mahasiswa2);
+	},
+	main: function(main) {
+		this.energi -= main;
+		console.log(Mahasiswa2);
+	}
+}
+
+
+// 2. Function Declaration
+/*function Mahasiswa(nama, energi) {
+	let mahasiswa = {};
+	mahasiswa.nama = nama;
+	mahasiswa.energi = energi;
+
+	mahasiswa.makan = function(porsi) {
+		this.energi = this.energi + porsi;
+		console.log(`Halo ${this.nama}, Selamat Makan!`);
+	}
+
+	return mahasiswa;
+}
+
+let sandhika = Mahasiswa('sandhika', 10);
+let doddy = Mahasiswa('doddy', 15);*/
+
+
+// Constructor Function
+// keyword new
+/*function Mahasiswa(nama, energi) {
+	this.nama = nama;
+	this.energi = energi;
+
+	this.makan = function (porsi) {
+		this.energi += porsi;
+		console.log(`halo ${this.nama}, selamat makan!`);
+	}
+
+	this.main = function (jam) {
+		this.energi -= jam;
+		console.log(`halo ${this.nama}, selamat bermain!`);
+	}
+}
+
+let sandhika = new mahasiswa('sandhika', 10);
+let doddy = new mahasiswa('sandhika', 20);*/
+
+
+// Function Declaration
+// object.create()
+const method = {
+	makan : function(porsi) {
+		this.energi = this.energi + porsi;
+		console.log(`Halo ${this.nama}, Selamat Makan!`);
+	},
+
+	main : function(jam) {
+		this.energi = this.energi - jam;
+		console.log(`Halo ${this.nama}, selamat bermain!`);
+	},
+
+	tidur : function() {
+		this.energi = this.energi * 2;
+		console.log(`Halo ${this.nama}, selamat beristirahat!`);
+	}
+}
+
+function Mahasiswa(nama, energi) {
+	let mahasiswa = {};
+	mahasiswa.nama = nama;
+	mahasiswa.energi = energi;
+	mahasiswa.makan = method.makan;
+	mahasiswa.main = method.main;
+	mahasiswa.tidur = method.tidur;
+
+	return mahasiswa;
+}
+
+let sandhika = Mahasiswa('sandhika', 10);
+let doddy = Mahasiswa('doddy', 15);
+let erik = Mahasiswa('erik', 25);
